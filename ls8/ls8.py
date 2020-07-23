908ee78952eae0c0ff31c2a49a90cc6a18238338
Cole Wilkison <wilkison.cole@gmail.com>
2020-07-23 13:55:59 -0400

    wednesday mvp

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -1,21 +1,12 @@
-#!/usr/bin/env python3
-
-"""Main."""
-
-# import sys
-# from cpu import *
-
-# cpu = CPU()
-
-# cpu.load(sys.argv[1])
-# cpu.run()
-
 import sys
 from os.path import join
 from cpu import *
+
 # filename = sys.argv[1]
 filename = open(join('./examples/', sys.argv[1]), 'r')
 code = filename.readlines()
 filename.close()
 cpu = CPU()
+
 cpu.load(code)
+cpu.run()
